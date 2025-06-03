52035204d22836644600f2d477b932f890fc88da
Ambrose Othniel <113441561+Broseey@users.noreply.github.com>
2025-06-03 04:54:17 +0100

    rounding components in form

--- src/components/RideBookingFormNew.tsx
+++ src/components/RideBookingFormNew.tsx
@@ -440,7 +440,7 @@ const RideBookingFormNew = () => {
                         value={field.value}
                       >
                         <FormControl>
-                          <SelectTrigger className="flex items-center hover:border-black transition-colors">
+                          <SelectTrigger className="flex items-center hover:border-black transition-colors rounded-[2rem]">
                             <Clock className="mr-2 h-5 w-5 text-gray-500" />
                             <SelectValue placeholder="Select a time" />
                           </SelectTrigger>
@@ -469,7 +469,7 @@ const RideBookingFormNew = () => {
                         value={field.value}
                       >
                         <FormControl>
-                          <SelectTrigger className="flex items-center hover:border-black transition-colors">
+                          <SelectTrigger className="flex items-center hover:border-black transition-colors rounded-2rem">
                             <Users className="mr-2 h-5 w-5 text-gray-500" />
                             <SelectValue placeholder="Select passengers" />
                           </SelectTrigger>
